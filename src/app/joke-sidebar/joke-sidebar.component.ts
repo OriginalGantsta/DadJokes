e51e254d941411e2a6b3315e09a623ce72706ea3
OriginalGantsta <73863785+OriginalGantsta@users.noreply.github.com>
2021-11-18 23:27:34 -0600

    Fixed functionality of undo button so it disappears when new joke is added.

--- src/app/joke-sidebar/joke-sidebar.component.ts
+++ src/app/joke-sidebar/joke-sidebar.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable} from 'rxjs';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 import { DatabaseService } from '../services/database.service';
 import { Joke } from '../Model/joke.model';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-joke-sidebar',
@@ -10,46 +11,45 @@ import { Joke } from '../Model/joke.model';
 })
 export class JokeSidebarComponent implements OnInit, OnDestroy {
   jokes: BehaviorSubject<Joke[]>;
+  jokesSubscription: Subscription;
   undoLocation: String | null;
   undoIndex: number;
+  jokeArrayLength: number;
 
-  constructor(
-    private databaseService: DatabaseService
-  ) {}
+  constructor(private databaseService: DatabaseService) {}
 
   removeFromFavorites(joke: Joke, i) {
-    console.log(i)
-    if (i < this.jokes.value.length -1){
-      this.undoLocation = "above";
+    if (i < this.jokes.value.length - 1) {
+      this.undoLocation = 'above';
       this.undoIndex = i;
-    }
-    else if (i === this.jokes.value.length - 1){
+    } else if (i === this.jokes.value.length - 1) {
       this.undoLocation = 'below';
       this.undoIndex = i - 1;
-    }
-    else if (this.jokes.value.length === 1){
+    } else if (this.jokes.value.length === 1) {
       this.undoIndex = -1;
     }
-    console.log(this.undoLocation);
-    console.log(this.undoIndex);
     this.databaseService.removeJoke(joke);
+    this.jokeArrayLength = this.jokes.value.length;
+
   }
 
-  undoRemoveFromFavorites(){
+  undoRemoveFromFavorites() {
     this.undoLocation = null;
     this.undoIndex = null;
     this.databaseService.saveFavoriteJoke(this.databaseService.removedJoke);
   }
 
-  favoriteJoke(joke) {
-      this.databaseService.saveFavoriteJoke(joke);
-    }
-
-
   ngOnInit(): void {
-    this.jokes = this.databaseService.jokes
+    this.jokes = this.databaseService.jokes;
+    this.jokesSubscription = this.jokes.pipe(
+      tap((jokes) => {
+        if (jokes.length >= this.jokeArrayLength) {
+          this.undoLocation = null;
+          this.undoIndex = null;
+        }
+      })
+    ).subscribe();
   }
 
-  ngOnDestroy() {
-  }
+  ngOnDestroy() {this.jokesSubscription.unsubscribe}
 }
